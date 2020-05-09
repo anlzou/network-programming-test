0a37e220af319aaabe7150b48a7659d29a1f1dfe
anlzou <599502931@qq.com>
2020-05-09 22:13:46 +0800

    feat(version 2.0):restructure the code

--- src/tcpClient.cpp
+++ src/tcpClient.cpp
@@ -1,13 +1,14 @@
-/***
+/*** 
  * @Date        : 2020-05-09 19:06:39
  * @LastEditors : anlzou
  * @Github      : https://github.com/anlzou
- * @LastEditTime: 2020-05-09 19:08:31
+ * @LastEditTime: 2020-05-09 22:10:09
  * @FilePath    : \tcp-socket\src\tcpClient.cpp
- * @Describe    :
+ * @Describe    : 
  */
 
 #include <stdio.h>
+#include <iostream>
 #include <winsock2.h>
 #include <windows.h>
 #pragma comment(lib, "WS2_32.lib")  //显示连接套接字
@@ -16,25 +17,39 @@ using namespace std;
 
 int main() {
     WSADATA data;             //定义WSADATA结构体对象
-    WORD w = MAKEWORD(2, 0);  //定义版本号
-    ::WSAStartup(w, &data);   //初始化套接字库
-    SOCKET s;                 //定义连接套接字句柄
+    WORD version = MAKEWORD(2, 0);  //定义版本号
+    WSAStartup(version, &data);   //初始化套接字库
+    SOCKET sockCli;                 //定义连接套接字句柄
     char sztext[10] = {0};
-    s = ::socket(AF_INET, SOCK_STREAM, 0);  //创建TCP套接字
-    sockaddr_in addr;                       //定义套接字地址结构
-    addr.sin_family = AF_INET;              //初始化地址结构
-    addr.sin_port = htons(75);
-    addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-    printf("Client started.\r\n");  //输出提示信息
-    ::connect(s, (sockaddr*)&addr, sizeof(addr));
-    ::recv(s, sztext, sizeof(sztext), 0);
-    printf("%s\r\n", sztext);
-    ::closesocket(s);  //关闭套接字句柄
-    ::WSACleanup();    //如果有输入，则关闭程序
-    if (getchar()) {
-        return 0;  //正常结束程序
-    } else {
-        ::Sleep(100);  //程序休眠
+    sockCli = socket(AF_INET, SOCK_STREAM, 0);  //创建TCP套接字
+    
+    sockaddr_in addrSer;                       //定义套接字地址结构
+    addrSer.sin_family = AF_INET;              //初始化地址结构
+    addrSer.sin_port = htons(75);
+    addrSer.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+
+    int res = connect(sockCli, (sockaddr*)&addrSer, sizeof(addrSer));
+    if (res != 0){
+        cout<<"Client Connect Server Fail!"<<endl;
+    }else{
+        cout<<"Client Connect Server Success!"<<endl;
     }
+
+    char sendbuf[256];
+    char recvbuf[256];
+    while (1)
+    {
+        recv(sockCli,recvbuf,256,0);
+        cout<<"Ser:>"<<recvbuf<<endl;
+
+        cout<<"Cli:>";
+        cin>>sendbuf;
+        if (strcmp(sendbuf,"quit") == 0){
+            break;
+        }
+        send(sockCli,sendbuf,strlen(sendbuf)+1,0);
+    }
+    closesocket(sockCli);
+    WSACleanup();
     return 0;
 }
--- src/tcpServer.cpp
+++ src/tcpServer.cpp
@@ -9,39 +9,55 @@
 #include <stdio.h>
 #include <winsock2.h>
 #include <windows.h>
-
-#pragma comment(lib, "WS2_32.lib")  //显示连接套接字库
+#include <iostream>
+#pragma comment(lib, "ws2_32.lib")  //引用连接套接字库
 using namespace std;
 
-int main() {                       //主函数开始
-    WSADATA data;                  // 定义WSADATA结构体
-    WORD w = MAKEWORD(2, 0);       //定义版本号
-    char sztext[] = "Welcome!\r\n";  //定义初始化发送到客户端的字符数组
-    ::WSAStartup(w, &data);        //初始化套接字
-    SOCKET s, s1;  //定义连接套接字和数据收发套接字句柄
-    s = ::socket(AF_INET, SOCK_STREAM, 0);  //创建TCP套接字
-    sockaddr_in addr, addr2;                //定义套接字地址结构
-    int n = sizeof(addr2);                  //获取套接字地址结构大小
-    addr.sin_family = AF_INET;              //初始化地址结构
-    addr.sin_port = htons(75);
-    addr.sin_addr.S_un.S_addr = INADDR_ANY;
-    ::bind(s, (sockaddr*)&addr, sizeof(addr));  //绑定套接字
-    ::listen(s, 5);                             //监听套接字
-    printf("Server started.\r\n");           //输出提示信息
+int main() {                         //主函数开始
+    WSADATA data;                    // 定义WSADATA结构体
+    WORD version = MAKEWORD(2, 0);         //定义版本号
+    WSAStartup(version, &data);            //初始化套接字，使socket适配Windows平台
 
-    while (true) {
-        s1 = ::accept(s, (sockaddr*)&addr2, &n);  //接受连接请求
-        if (s1 != NULL) {
-            printf("%s Connected.\r\n", inet_ntoa(addr2.sin_addr));
-            ::send(s1, sztext, sizeof(sztext), 0);  //向客户端发送字符数组
-        }
-        ::closesocket(s);
-        ::closesocket(s1);
-        ::WSACleanup;     //释放套接字库
-        if (getchar()) {  //如果有输入，则关闭程序
-            return 0;     //正常结束
-        } else {
-            ::Sleep(100);  //睡眠0.1秒
-        }
+    SOCKET sockSer;  //定义连接套接字和数据收发套接字句柄
+    sockSer = socket(AF_INET, SOCK_STREAM, 0);  //创建TCP套接字
+
+    sockaddr_in addrSer, addrCli;  //定义套接字地址结构，服务端；客户端
+    addrSer.sin_family =
+        AF_INET;  //初始化地址结构；AF_INET:家族协议，默认2，internetwork：UDP，TCP
+    addrSer.sin_port = htons(75);               //监听端口
+    addrSer.sin_addr.S_un.S_addr = INADDR_ANY;  // inet_addr("127.0.0.1")
+
+    bind(sockSer, (sockaddr*)&addrSer, sizeof(addrSer));  //绑定套接字
+    listen(sockSer, 5);                                   //监听套接字
+
+    // printf("Server started.\r\n");  //输出提示信息
+    cout << "Server wait client connect..." << endl;
+
+    SOCKET sockConn;  //客户端的连接
+    int len = sizeof(SOCKADDR);
+    sockConn = accept(sockSer, (SOCKADDR*)&addrCli, &len);
+
+    if (sockConn == INVALID_SOCKET) {
+        printf("Server Accept Client Connect Fail!\r\n");
+        return 0;
+    } else {
+        printf("Server Accept Client Connect Success!\r\n");
     }
+
+    char sendbuf[256];
+    char recvbuf[256];
+    while (1) {
+        cout << "Server:>";
+        cin >> sendbuf;
+        if (strcmp(sendbuf, "quit") == 0) {
+            break;
+        }
+        send(sockConn, sendbuf, strlen(sendbuf) + 1, 0);  //+1=字符串\0; 0，代表默认方式发送
+        recv(sockConn, recvbuf, 256, 0);        // 0，代表默认方式接受
+        cout << "Client:>" << recvbuf << endl;  //输出客户端的内容
+    }
+    closesocket(sockSer);
+    closesocket(sockConn);
+    WSACleanup();
+    return 0;
 }
