1596ade9dbf0d22c32d8b50b8104a0e34f9bf839
anlzou <599502931@qq.com>
2020-05-25 08:18:02 +0800

    docs(test/03/test3_3/):run on vs code

--- src/test/03/test3_3/NamedPipe/管道客户端.cpp
+++ src/test/03/test3_3/NamedPipe/管道客户端.cpp
@@ -1,28 +1,37 @@
-#include<iostream>
-#include<windows.h>
+/***
+ * @Date        : 2020-05-23 18:55:57
+ * @LastEditors : anlzou
+ * @Github      : https://github.com/anlzou
+ * @LastEditTime: 2020-05-25 08:11:16
+ * @FilePath    : \socket\src\test\03\test3_3\NamedPipe\管道客户端.cpp
+ * @Describe    :
+ */
+#include <iostream>
+#include <windows.h>
 
 using namespace std;
 
-int main(){
-	HANDLE hpip;
-	OVERLAPPED ovi = {0};
-	char buf[] = "命名管道测试程序";
-	DWORD readbuf;
-	cout<<"正在连接命名管道"<<endl;
+int main() {
+    HANDLE hpip;
+    OVERLAPPED ovi = {0};
+    char buf[] = "命名管道测试程序";
+    DWORD readbuf;
+    cout << "正在连接命名管道" << endl;
 
-	if(WaitNamedPipe("\\\\.\\pipe\\pipename",NMPWAIT_WAIT_FOREVER)){
-		hpip = CreateFile("\\\\.\\pipe\\pipename",GENERIC_READ|GENERIC_WRITE,0,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,NULL);
-		if(hpip == INVALID_HANDLE_VALUE){
-			cout<<"打开命名管道失败"<<endl;
-		}else{
-			if(WriteFile(hpip,buf,sizeof(buf),&readbuf,NULL)){
-				cout<<"数据写入成功"<<endl;
-			}else{
-				cout<<"数据写入失败"<<endl;
-			}
-		}
-	}else{
-		cout<<"连接命令管道失败"<<endl;
-	}
-	return 0;
+    if (WaitNamedPipe("\\\\.\\pipe\\pipename", NMPWAIT_WAIT_FOREVER)) {
+        hpip = CreateFile("\\\\.\\pipe\\pipename", GENERIC_READ | GENERIC_WRITE,
+                          0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+        if (hpip == INVALID_HANDLE_VALUE) {
+            cout << "打开命名管道失败" << endl;
+        } else {
+            if (WriteFile(hpip, buf, sizeof(buf), &readbuf, NULL)) {
+                cout << "数据写入成功" << endl;
+            } else {
+                cout << "数据写入失败" << endl;
+            }
+        }
+    } else {
+        cout << "连接命令管道失败" << endl;
+    }
+    return 0;
 }
--- src/test/03/test3_3/NamedPipe/管道服务端.cpp
+++ src/test/03/test3_3/NamedPipe/管道服务端.cpp
@@ -1,26 +1,36 @@
-#include<iostream>
-#include<windows.h>
+/***
+ * @Date        : 2020-05-23 18:55:57
+ * @LastEditors : anlzou
+ * @Github      : https://github.com/anlzou
+ * @LastEditTime: 2020-05-25 08:10:42
+ * @FilePath    : \socket\src\test\03\test3_3\NamedPipe\管道服务端.cpp
+ * @Describe    :
+ */
+#include <iostream>
+#include <windows.h>
 
 using namespace std;
 
-int mainxx(){
-	HANDLE hpip;
-	OVERLAPPED ovi = {0};
-	char buf[200];
-	DWORD readbuf;
-	hpip = CreateNamedPipe("\\\\.\\pip\\pipename",PIPE_ACCESS_DUPLEX,PIPE_TYPE_BYTE,PIPE_UNLIMITED_INSTANCES,1024,1024,0,NULL);
-	cout<<"create sucess"<<endl;
+int main() {
+    HANDLE hpip;
+    OVERLAPPED ovi = {0};
+    char buf[200];
+    DWORD readbuf;
+    hpip = CreateNamedPipe("\\\\.\\pip\\pipename", PIPE_ACCESS_DUPLEX,
+                           PIPE_TYPE_BYTE, PIPE_UNLIMITED_INSTANCES, 1024, 1024,
+                           0, NULL);
+    cout << "create sucess" << endl;
 
-	cout<<"正在等待客户端的连接。。。"<<endl;
-	if(ConnectNamedPipe(hpip,&ovi)){
-		cout<<"客户端连接成功!!!!!"<<endl;
-		cout<<"正在读取数据"<<endl;
-		if(ReadFile(hpip,buf,200,&readbuf,NULL)){
-			cout<<"读取数据成功！！！！！<<endl";
-			cout<<"数据为："<<buf<<endl;
-		}else{
-			cout<<"读取数据失败！！！！！！！！";
-		}
-	}
-	return 0;
+    cout << "正在等待客户端的连接。。。" << endl;
+    if (ConnectNamedPipe(hpip, &ovi)) {
+        cout << "客户端连接成功!!!!!" << endl;
+        cout << "正在读取数据" << endl;
+        if (ReadFile(hpip, buf, 200, &readbuf, NULL)) {
+            cout << "读取数据成功！！！！！<<endl";
+            cout << "数据为：" << buf << endl;
+        } else {
+            cout << "读取数据失败！！！！！！！！";
+        }
+    }
+    return 0;
 }
